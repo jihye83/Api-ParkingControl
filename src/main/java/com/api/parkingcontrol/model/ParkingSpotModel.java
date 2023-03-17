d7b18ec5e1c2b16228cb698c1182b1e1ccf717dc
jihye <jihye83@hotmail.com>
2023-03-17 18:13:51 -0300

    controller

--- src/main/java/com/api/parkingcontrol/model/ParkingSpotModel.java
+++ src/main/java/com/api/parkingcontrol/model/ParkingSpotModel.java
@@ -25,28 +25,20 @@ public class ParkingSpotModel implements Serializable {
 
 	@Column(nullable = false, unique = true, length = 10)
 	private String parkingSpotNumber;
-
 	@Column(nullable = false, unique = true, length = 7)
 	private String licensePlateCar;
-
 	@Column(nullable = false, length = 70)
 	private String brandCar;
-
 	@Column(nullable = false, length = 70)
 	private String modelCar;
-
 	@Column(nullable = false, length = 70)
 	private String colorCar;
-
 	@Column(nullable = false)
 	private LocalDateTime registrationDate;
-
 	@Column(nullable = false, length = 130)
 	private String responsibleName;
-
 	@Column(nullable = false, length = 30)
 	private String apartment;
-
 	@Column(nullable = false, length = 30)
 	private String block;
 
--- src/main/java/com/api/parkingcontrol/repository/ParkingSpotRepository.java
+++ src/main/java/com/api/parkingcontrol/repository/ParkingSpotRepository.java
@@ -1,6 +1,7 @@
 package com.api.parkingcontrol.repository;
 
 import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,7 @@ import com.api.parkingcontrol.model.ParkingSpotModel;
 
 @Repository
 public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
-	
+    boolean existsByLicensePlateCar(String licensePlateCar);
+    boolean existsByParkingSpotNumber(String parkingSpotNumber);
+    boolean existsByApartmentAndBlock(String apartment, String block);
 }
